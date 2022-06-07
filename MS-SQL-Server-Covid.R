b2370ed297b281c71b7f226ba8d290a4f1711b2c
davidjayjackson <davidjayjackson@gmail.como>
2022-06-07 19:57:48 -0400

    Changed code to use the mutate function in Tidyverse 2022-06-07

--- MS-SQL-Server-Covid.R
+++ MS-SQL-Server-Covid.R
@@ -17,7 +17,8 @@ rm(list=ls())
 retail_sales <- read_xlsx("./Online Retail.xlsx")
 retail_sales <- retail_sales %>% janitor::clean_names()
 retail_sales <- retail_sales %>% janitor::remove_empty(which = c("rows","cols"))
-USA$date <- as.Date(USA$date)
+# retail_sales$total_sales <- retail_sales$quantity * retail_sales$unit_price
+retail_sales <- mutate(retail_sales,total_sales =quantity * unit_price)
 
 ## https://db.rstudio.com/databases/microsoft-sql-server/
 con <- DBI::dbConnect(odbc::odbc(), 
@@ -32,10 +33,10 @@ con <- DBI::dbConnect(odbc::odbc(),
 # USA <- read.csv("../COVID-19-NYTimes-data//us.csv")
 # USA$date <- as.Date(USA$date)
 
-dbWriteTable(con, "Covid",USA ,overwrite=TRUE)
+dbWriteTable(con, "retail_sales", retail_sales,overwrite=TRUE)
 dbListFields(con,"Covid")
 
-dbGetQuery(con,"select max(date) from Covid")
+dbGetQuery(con,"select count(*) as row_count from retail_sales")
 # dbCommit(con)
 
 dbDisconnect(con)
